bf7807562c2c0e1d92251489f0b32d0343c56401
Alvaro Ortiz Pedroso <alvaro.ortiz.pedroso@gmail.com>
2023-02-27 14:55:27 +0100

    first commit

---
+++ src/main/java/com/huevitos/springboot/web/app/HuevitosSpringApplication.java
@@ -0,0 +1,13 @@
+package com.huevitos.springboot.web.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HuevitosSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HuevitosSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/huevitos/springboot/web/app/HuevitosSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.huevitos.springboot.web.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HuevitosSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
