044bfee975c6823f982c1555317698b9b069c3aa
Alvaro Ortiz Pedroso <alvaro.ortiz.pedroso@gmail.com>
2023-02-27 15:32:31 +0100

    security

--- src/main/java/com/huevitos/springboot/web/app/controller/LoginController.java
+++ src/main/java/com/huevitos/springboot/web/app/controller/LoginController.java
@@ -1,5 +1,23 @@
 package com.huevitos.springboot.web.app.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.huevitos.springboot.web.app.entity.User;
+
+@RestController
+@RequestMapping("login")
 public class LoginController {
 
+	@GetMapping
+	public List<User> listUsers(){
+		List<User> result = new ArrayList<>();
+		result.add(new User(1, "Paco", "email", "pass"));
+		//https://www.youtube.com/watch?v=_p-Odh3MZJc
+		return result;
+	}
 }
--- src/main/java/com/huevitos/springboot/web/app/entity/User.java
+++ src/main/java/com/huevitos/springboot/web/app/entity/User.java
@@ -1,19 +1,64 @@
 package com.huevitos.springboot.web.app.entity;
 
-import java.math.BigDecimal;
-
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 
 @Entity
-@Table(name="usuario")
+@Table(name = "usuario")
 public class User {
-	
+
 	@Id
-	private BigDecimal usuarioId;
-	
-	private String algo;
-	
-	private String algo2;
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer usuarioId;
+
+	private String nombre;
+	private String email;
+	private String password;
+
+	public User() {
+
+	}
+
+	public User(Integer usuarioId, String nombre, String email, String password) {
+		this.usuarioId = usuarioId;
+		this.nombre = nombre;
+		this.email = email;
+		this.password = password;
+	}
+
+	public Integer getUsuarioId() {
+		return usuarioId;
+	}
+
+	public void setUsuarioId(Integer usuarioId) {
+		this.usuarioId = usuarioId;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
 }
